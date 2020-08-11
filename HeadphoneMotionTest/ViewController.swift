35d2af27ab3fdafe40ac30f0a7dab45692e7c497
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-08-11 15:47:45 +0200

    Initial Commit

---
+++ HeadphoneMotionTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeadphoneMotionTest
+//
+//  Created by Koki Ibukuro on 2020/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
