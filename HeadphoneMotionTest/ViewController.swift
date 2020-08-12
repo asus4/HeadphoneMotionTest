14cf99cafa4c9d3fdc51a6e97e514345682ea3f8
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-08-12 17:36:24 +0200

    Test the API

--- HeadphoneMotionTest/ViewController.swift
+++ HeadphoneMotionTest/ViewController.swift
@@ -9,13 +9,40 @@
 // https://developer.apple.com/forums/thread/650051
 
 import UIKit
+import CoreMotion
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CMHeadphoneMotionManagerDelegate {
+    
+    let motionManager = CMHeadphoneMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("hogehoge")
+        
+        motionManager.delegate = self
+        
+        print("is active", motionManager.isDeviceMotionActive)
+        print("is available", motionManager.isDeviceMotionAvailable)
+        
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates(to: .main)  { (motion: CMDeviceMotion?, error: Error?) in
+                print("error \(error)")
+                print("motion \(motion)")
+            }
+        }
+    }
+    
+    deinit {
+        motionManager.stopDeviceMotionUpdates()
+    }
+    
+    func headphoneMotionManagerDidConnect(_ manager: CMHeadphoneMotionManager) {
+        print("connect")
+    }
+    
+    func headphoneMotionManagerDidDisconnect(_ manager: CMHeadphoneMotionManager) {
+        print("disconnect")
     }
 
 
